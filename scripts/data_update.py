6dd93af7dcf7ec76a6704547cdb38b0cc35e8113
luizatebesoi <66624270+luizatebesoi@users.noreply.github.com>
2020-09-20 21:57:40 +0300

    test

--- scripts/data_update.py
+++ scripts/data_update.py
@@ -1,4 +1,21 @@
+import os
+from os.path import dirname
+
 import data_processing_functions_final as dpf
 import images_download as dwn
+import pandas as pd
 
-dpf.image_titles(["https://images.deliveryhero.io/image/fd-ro/LH/v3gk-listing.jpg"])
+
+def image_titles1(links):
+    results = []
+    for link in links:
+        if pd.notna(link):
+            filename = link.split("/")[-1]
+            file = os.path.join(os.path.join(dirname(dirname(__file__)), "media/restaurants/images/"), filename)
+            file = file.replace("\\","/")
+            results.append(file)
+        else:
+            results.append(None)
+    print(file)
+
+image_titles1(["https://images.deliveryhero.io/image/fd-ro/LH/v3gk-listing.jpg"])
